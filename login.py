ac1c2d57855a9f30629d9969a0fa62890f02d4b0
192524404 <192524404.simats@saveetha.com>
2026-08-05 14:41:17 +0530

    Update login.py

--- login.py
+++ login.py
@@ -1,6 +1,6 @@
 def login(username, password):
     if username == "admin" and password == "admin123":
         return "Login Successful"
-    return "Invalid Username or Password"
+    return "Invalid Username or Password";
 
 print(login("admin", "admin123"))
